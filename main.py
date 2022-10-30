97c2d9bb513ee91d0704ead0628c60fbd55495a7
Вера Куприянова <veruna13@mail.ru>
2022-10-30 10:54:56 +0200

    code docstring

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Выполнение атаки."""
+    ...
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +16,8 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Выполнение."""
+    ...
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +27,8 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Специальное умение."""
+    ...
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +39,8 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Описание игрока."""
+    ...
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +65,8 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор игрока."""
+    ...
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -79,6 +89,8 @@ def choice_char_class() -> str:
 
 
 def main() -> str:
+    """Основная функция."""
+    ...
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
